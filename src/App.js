5d5355cb255f37161f4336e5f7b030ed1d13a997
Pushpankar <pushpankark@gmail.com>
2018-03-29 00:50:11 +0530

    Create simple todo

--- src/App.js
+++ src/App.js
@@ -6,20 +6,34 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            count: 0
+            todos: [],
+            currentTodo: ""
         };
-
     }
 
-    increment = () => {
-        this.setState({count: this.state.count + 1});
+    addTodo = task => {
+        let todosCopy = this.state.todos.slice();
+        todosCopy.push(this.state.currentTodo);
+        this.setState({todos: todosCopy,
+                       currentTodo: ""});
     }
 
-    render(){
+    onInputChange = e => {
+        this.setState({ currentTodo : e.target.value });
+    }
+
+    render() {
+        let bulletedTodos = this.state.todos.map((e, i) => {
+            return (
+                    <li key={i}>{e}</li>
+            );
+        });
         return (
                 <div>
-                <button onClick={this.increment}>Increment</button>
-                    {this.state.count}
+                <input placeholder="Enter todo" value={this.state.currentTodo} onChange={this.onInputChange}/>
+                <button onClick={this.addTodo}>Add!</button>
+                <br/>
+                {this.state.todos.length == 0 ? "No Todos yet" : <ul>{bulletedTodos}</ul>}
                 </div>
         );
     }
